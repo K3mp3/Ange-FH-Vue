00569caa576c4fb279e57f2a33cc646aebffb500
Melvin Kempe <k3mp314@gmail.com>
2023-04-27 21:20:43 +0200

    [Added] code to display admin tool

--- Client/src/scripts/frontpage.ts
+++ Client/src/scripts/frontpage.ts
@@ -1,8 +1,11 @@
+const toolContainer = document.createElement("div");
+
 
 export default function showFrontPage() {
     const accountForm = document.querySelector(".account-form") as HTMLDivElement;
     console.log("accountForm", accountForm);
-    
     accountForm.innerHTML = "";
+
+    
 }
 
