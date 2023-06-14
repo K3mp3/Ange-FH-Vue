15704fc397f94595332695a480be60bea6b13b62
Melvin Kempe <k3mp314@gmail.com>
2023-06-14 18:59:34 +0200

    [Updated] Main with all changes

---
+++ Client/src/components/adminpage/AdminPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    
+</script>
+<template>
+    <div class="small-container">
+        <input type="file" />
+    </div>
+</template>
+<style scoped>
+    .small-container {
+        background-color: aliceblue;
+        padding: 20px;
+    }
+</style>
