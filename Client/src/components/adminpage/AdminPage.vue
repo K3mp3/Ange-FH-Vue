255686c21a0ba6cea01b4e55e18039d51d7c07ef
Melvin Kempe <k3mp314@gmail.com>
2023-06-15 15:01:28 +0200

    [Added] Axios, file input for the movie posters and a button to save the poster. I also added a emit to send the poster to the parent component to be able to save the poster in the database from that component

--- Client/src/components/adminpage/AdminPage.vue
+++ Client/src/components/adminpage/AdminPage.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
-    
+    import { ref } from 'vue';
+
+    const selectedFile = ref<File | null>(null);
+    const imageUrl = ref<string | null>(null);
+
+
+    function fileInput(e: Event) {
+        const inputElement = e.target as HTMLInputElement;
+        selectedFile.value = inputElement.files ? inputElement.files[0] : null
+
+        //console.log(selectedFile.value);
+        if (selectedFile.value) {
+            imageUrl.value = URL.createObjectURL(selectedFile.value);
+        }
+    }
+
+    const emits = defineEmits<{ (e: "moviePoster", poster: FormData): void }>();
+
+    async function emitImage() {
+        if (!selectedFile.value) {
+            //console.log("No file selected.");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("image", selectedFile.value)
+        //console.log(formData);
+
+        emits ("moviePoster", formData)
+    }
 </script>
 <template>
     <div class="small-container">
-        <input type="file" />
+        <input type="file" @change="fileInput"/>
+        <button accept="image/jpeg, image/png, image/jpg" @click="emitImage">Upload image</button>
+        <!-- <img :src="imageUrl" alt="Uploaded Image" v-if="imageUrl" /> -->
     </div>
 </template>
 <style scoped>
--- Client/src/views/AdminView.vue
+++ Client/src/views/AdminView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import AdminPageVue from '@/components/adminpage/AdminPage.vue';
+    import AdminPageVue from '@/components/adminpage/AdminPage.vue';
 
- 
+    function saveMoviePoster(moviePoster: FormData) {
+        console.log("moviePoster", moviePoster);
+    }
 </script>
 
 <template>
- <AdminPageVue></AdminPageVue>
+ <AdminPageVue @moviePoster="saveMoviePoster"></AdminPageVue>
 </template>
 
 <style scoped>
