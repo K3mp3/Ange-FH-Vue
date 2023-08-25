2986f08c5ee1a77a33cca73c48bcabe4853043be
Melvin Kempe <k3mp314@gmail.com>
2023-08-25 15:09:53 +0200

    [Added] route.params.id to a reactive variable to be able to read out the id of which poster the user clicks on

--- Client/src/components/movies/MovieInfo.vue
+++ Client/src/components/movies/MovieInfo.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute(); // Get the current route object
+const id = ref(route.params.id);
+console.log(id.value);
 </script>
 
 <template>
-    <p>Hello</p>
+    <div>
+        <h2>Movie Details</h2>
+        <p>ID: {{ id }}</p>
+        <!-- Fetch and display movie details based on the ID -->
+    </div>
 </template>
 
 <style scoped>
-</style>
+    h2, p {
+        color: #fff;
+    }
+</style>    
