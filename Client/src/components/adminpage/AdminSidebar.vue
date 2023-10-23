922dfb193c3ff14c76b248c9dd28200e0fdc53a9
Melvin Kempe <k3mp314@gmail.com>
2023-10-23 21:07:31 +0200

    [Added] place the nav 100vw outside of the screen when not showing and then slide it back in when showing

--- Client/src/components/adminpage/AdminSidebar.vue
+++ Client/src/components/adminpage/AdminSidebar.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
     import { ref } from 'vue';
+    import { RouterLink } from 'vue-router';
+    import gsap from 'gsap';
 
     const isNavOpen = ref(false);
+    const navElement = ref<HTMLElement | null>(null)
 
     function openNav() {
         isNavOpen.value = !isNavOpen.value;
+
+        console.log(isNavOpen.value)
+        if (navElement.value) {
+            if (isNavOpen.value) {
+            gsap.to(navElement, { opacity: 1, x: 0, duration: 0.3 });
+            } else {
+            gsap.to(navElement, { opacity: 0, x: -100, duration: 0.3 });
+            }
+        }
     }
 </script>
 
@@ -19,45 +31,62 @@
         <span :class="[isNavOpen ? 'span-open' : 'span']"></span>
         <span :class="[isNavOpen ? 'span-open' : 'span']"></span>
     </button>
-    <nav :class="[isNavOpen ? 'expended-nav' : 'nav']">
+    <nav :class="[isNavOpen ? 'expended-nav' : 'nav']" ref="expandedNav">
+        <li :class="[isNavOpen ? 'li' : 'hide']"><RouterLink to="/">Trailer</RouterLink></li>
+        <li :class="[isNavOpen ? 'li' : 'hide']"><RouterLink to="/">Trailer</RouterLink></li>
+        <li :class="[isNavOpen ? 'li' : 'hide']"><RouterLink to="/">Trailer</RouterLink></li>
     </nav>
 </template>
 
 <style scoped lang="scss">
     nav {
-        transition: all 0.2s ease-in-out;
-        width: 60px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding-top: 20px;
-        position: fixed;
-        top: 0;
-    }
-
-    .expended-nav {
-        transition: all 0.2s ease-in-out;
+        transition: all 0.3s ease-in-out;
+        left: -100vw;
         width: 100vw;
         height: 100vh;
         background-color: aqua;
         display: flex;
         flex-direction: column;
-        align-items: end;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+        position: fixed;
+        top: 0;
+        visibility: visible;
+    }
+
+    .expended-nav {
+        transition: all 0.3s ease-in-out;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: aqua;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
         padding: 20px;
         position: fixed;
         top: 0;
+        visibility: visible;
+    }
+
+    .hide {
+        display: none;
     }
 
     .hamburger-btn {
-        transition: all 0.2s ease-in-out;
+        transition: all 0.3s ease-in-out;
         margin-left: 20px;
         background: transparent;
         border: none;
         position: fixed;   
+        z-index: 1;
     }
 
     .menu-open-right {
-        transition: all 0.2s ease-in-out;
+        transition: all 0.3s ease-in-out;
         margin-left: 20px;
         background: transparent;
         border: none;
@@ -67,7 +96,7 @@
         transform: rotate(40deg);
     }
     .menu-open-left {
-        transition: all 0.2s ease-in-out;
+        transition: all 0.3s ease-in-out;
         margin-left: 20px;
         background: transparent;
         border: none;
@@ -78,7 +107,7 @@
     }
 
     span {
-        transition: all 0.2s ease-in-out;
+        transition: all 0.3s ease-in-out;
         width: 7px;
         height: 7px;
         margin: 7px;
@@ -88,7 +117,7 @@
     }
 
     .span-open {
-        transition: all 0.2s ease-in-out;
+        transition: all 0.3s ease-in-out;
         width: 7px;
         height: 7px;
         margin: 7px;
