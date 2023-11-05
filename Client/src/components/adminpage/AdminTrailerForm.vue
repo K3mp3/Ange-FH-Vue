3b1ba26b6e7db0219190a640c165c5b08ea03974
Melvin Kempe <k3mp314@gmail.com>
2023-11-05 15:28:09 +0100

    [Added] design for the upload trailer button

--- Client/src/components/adminpage/AdminTrailerForm.vue
+++ Client/src/components/adminpage/AdminTrailerForm.vue
@@ -3,9 +3,20 @@
 </script>
 
 <template>
-    <button type="button"><FontAwesome :icon="['fas', 'upload']" style="color: #ffffff;" /> Upload trailer</button>
+    <button type="button"><FontAwesome :icon="['fas', 'upload']" style="color: #ffffff;" /> Upload trailer image</button>
 </template>
 
 <style setup lang="scss">
-
+    button {
+    background-color: #FF7B0F;
+    border: none;
+    width: 100%;
+    height: 33px;
+    border-radius: 15px;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+}
 </style>
--- Client/src/components/adminpage/AdminTrailerHero.vue
+++ Client/src/components/adminpage/AdminTrailerHero.vue
@@ -47,6 +47,7 @@
         </template>
       </Carousel>
     </div>
+    
 </template>
   
 
