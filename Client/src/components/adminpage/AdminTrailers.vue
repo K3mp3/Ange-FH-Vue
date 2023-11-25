7906f3d694d65320de9854a7d74633d040798725
Melvin Kempe <k3mp314@gmail.com>
2023-11-25 02:42:07 +0100

    [Added] styling for screens up to 3000px in width for the carousel, should work for screens up to 5000px in width wighout problems

--- Client/src/components/adminpage/AdminTrailers.vue
+++ Client/src/components/adminpage/AdminTrailers.vue
@@ -24,9 +24,10 @@
 
     function scrollRight() {
         updateScreenSize();
-        scroll(width + 10)    
+        scroll(width)  
         
-        if (width >= 360) scroll(width + 16)
+        if (width >= 360) scroll(width - 33)
+        // if (width >= 1090) scroll(width + 20)
     }
 
     function scrollLeft() {
@@ -34,7 +35,8 @@
 
         scroll(-width - 10)
 
-        if (width >= 360) scroll(-width - 16)
+        if (width >= 360) scroll(-width)
+        
     }
 
     async function getTrailer() {
@@ -85,6 +87,40 @@
         return formattedDuration;
     };
 
+    function settings() {
+        return {
+            itemsToShow: 1,
+            snapAlign: 'center',
+        }
+    }
+
+    const breakpoints = {
+        360: {
+            itemsToShow: 2,
+            snapAlign: 'start',
+        },
+
+        660: {
+            itemsToShow: 3.5,
+            snapAlign: 'center',
+        },
+
+        960: {
+            itemsToShow: 5.5,
+            snapAlign: 'center',
+        },
+
+        1260: {
+            itemsToShow: 7.5,
+            snapAlign: 'center',
+        },
+
+        3000: {
+            itemsToShow: 9.5,
+            snapAlign: 'center',
+        },
+    }
+
     onMounted(() => {
         getTrailer();
         updateScreenSize()
@@ -93,7 +129,7 @@
 </script>
 
 <template>
-    <div class="scroll-buttons">
+    <!-- <div class="scroll-buttons">
         <button type="button" @click="scrollLeft()">Scroll Left</button>
         <button type="button" @click="scrollRight()">Scroll Right</button>
     </div>
@@ -105,7 +141,7 @@
                     :alt="index.title"
                 />
             </div>
-            <!-- <div class="trailer-info">
+            <div class="trailer-info">
                 <h2>{{ index.title }}</h2>
                 <ul>
                     <li>{{ index.genre }}</li>
@@ -121,13 +157,47 @@
                 <FontAwesome  class="fontawesome-icon" :icon="['fas', 'trash']" />
                     Delete
                 </button>
-            </div> -->
+            </div>
         </div>
-    </div>
+    </div> -->
+    <div class="carousel-container">
+      <Carousel :settings="settings" :breakpoints="breakpoints" :wrap-around="true">
+        <Slide v-for="slide in trailers" :key="slide._id">
+            <div class="slider-container">
+                <div class="image-container">
+                    <img
+                        :src="`http://localhost:3000/trailer/image/${slide.poster}`"
+                        :alt="slide.title"
+                    />
+                </div>
+                <div class="trailer-info">
+                    <h2>{{ slide.title }}</h2>
+                    <ul>
+                        <li>{{ slide.genre }}</li>
+                        <li>{{ slide.age }}</li>
+                        <li v-if="slide.duration">{{ formatDuration(slide.duration) }}</li>
+                    </ul>
+                    <ul>
+                        <li class="white-li">{{ formatReleaseDate(slide.date) }}</li>
+                        <li class="white-li">|</li>
+                        <li class="white-li">{{ slide.time }}</li>
+                    </ul>
+                    <button type="submit">                
+                    <FontAwesome  class="fontawesome-icon" :icon="['fas', 'trash']" />
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </Slide>
+        <template #addons>
+          <navigation />
+        </template>
+      </Carousel>
+    </div> 
 </template>
   
 <style scoped lang="scss">
-    h2 {
+    /* h2 {
         font-family: Arial;
         color: #F1F1F1;
         font-weight: 700;
@@ -137,13 +207,11 @@
         width: 100%;
         display: flex;
         overflow: auto;
-        gap: 10px;
         background-color: #FF7B0F;
 
         .slide-container {
             display: flex;
             flex-direction: column;
-            gap: 10px;
             background-color: aquamarine;
             .image-container {
                 width: calc(100vw);
@@ -205,15 +273,14 @@
 
     @media screen and (min-width: 360px) {
         .media-container {
-            gap: 15px;
-
+            padding-left: 15px;
             .slide-container {
 
+                border: 1px solid red;
                 .image-container {
-                    width: calc(50vw - 7px);
-                    /* max-width: 310px; */
+                    width: calc(50vw - 18px);
                     height: 25vw;
-                    /* max-height: 170px; */
+                    padding: 5px 5px;
                 }
             }
         }
@@ -224,10 +291,8 @@
 
             .slide-container {
                 .image-container {
-                    width: calc(33.33vw - 10px);
-                    /* max-width: 310px; */
+                    width: calc(33.33vw - 2px);
                     height: 16.5vw;
-                    /* max-height: 170px; */
                 }
             }
         }
@@ -235,12 +300,11 @@
 
     @media screen and (min-width: 1090px) {
         .media-container {
-            padding: 0px 20px;
-            gap: 20px;
+            padding: 0px 0px 0px 20px;
             .slide-container {
 
                 .image-container {
-                    width: calc(25vw - 25px);
+                    width: calc(25vw - 7px);
                     height: 12.5vw;
                     padding: 10px 15px;
                 }
@@ -255,9 +319,204 @@
 
                 .image-container {
                     width: calc(20vw - 24px);
-                    /* max-width: 310px; */
                     height: 10vw;
-                    /* max-height: 170px; */
+                }
+            }
+        }
+    } */
+    h2 {
+        font-family: Arial;
+        color: #F1F1F1;
+        font-weight: 700;
+        font-size: 1.2rem;
+    }
+
+  .carousel-container {
+    width: 100%;
+    overflow: hidden;
+    
+    .carousel__slide {
+        display: block;
+    }
+    
+    .carousel__prev,
+    .carousel__next {
+        box-sizing: content-box;
+        border: 5px solid #F1F1F1
+    }
+
+    .slider-container {
+        background-color: aqua;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px;
+        width: 100vw;
+
+        .image-container {
+            width: 100%;
+            height: 50vw;
+            border-radius: 8px;
+            border: 1px solid #2C2C2C;
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                border-radius: 8px;
+            }
+        }
+    }
+
+    .trailer-info {
+        width: 100%;
+        margin-top: 10px;
+        text-align: left;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+
+        ul {
+            display: flex;
+            gap: 11px;
+            padding: 0;
+
+            li {
+                list-style: none;
+                font-family: Arial;
+                color: #FF7B0F; 
+            }
+
+            .white-li {
+                list-style: none;
+                font-family: Arial;
+                color: #F1F1F1
+            }
+        }
+
+        button {
+            width: 110px;
+            border-radius: 20px;
+            border: none;
+            display: flex;
+            gap: 5px;
+            padding: 10px;
+            align-items: center;
+            justify-content: center;
+            background-color: #ff7b0f;
+            font-family: Verdana;
+            color: #F1F1F1;
+            margin-bottom: 10px;
+        }
+    }  
+  }
+
+    @media screen and (min-width: 360px) {
+        .carousel-container {
+            background-color: #FF7B0F;
+
+            .slider-container {
+                width: calc(50vw - 18px);
+                border: 1px solid red;
+                margin: auto;
+                
+                .image-container {
+                    width: calc(50vw - 22px);
+                    height: 25vw;
+                    display: flex;
+                    align-items: center;
+
+                    img {
+                        width: 100%;
+                    }
+                }
+            }
+        }
+    }
+
+    @media screen and (min-width: 660px) {
+        .carousel-container {
+
+            .slider-container {
+                width: calc(28.57vw - 18px);
+                border: 1px solid red;
+                margin: auto;
+
+                .image-container {
+                    width: calc(28.57vw - 22px);
+                    height: 16.5vw;
+                    display: flex;
+                    align-items: center;
+
+                    img {
+                        width: 100%;
+                    }
+                }
+            }
+        }
+    }
+
+    @media screen and (min-width: 960px) {
+        .carousel-container {
+
+            .slider-container {
+                width: calc(18.18vw - 18px);
+                border: 1px solid red;
+                margin: auto;
+
+                .image-container {
+                    width: calc(18.18vw - 22px);
+                    height: 9.9vw;
+                    display: flex;
+                    align-items: center;
+
+                    img {
+                        width: 100%;
+                    }
+                }
+            }
+        }
+    }
+
+    @media screen and (min-width: 960px) {
+        .carousel-container {
+
+            .slider-container {
+                width: calc(13.33vw - 18px);
+                border: 1px solid red;
+                margin: auto;
+
+                .image-container {
+                    width: calc(13.33vw - 22px);
+                    height: 6.665vw;
+                    display: flex;
+                    align-items: center;
+
+                    img {
+                        width: 100%;
+                    }
+                }
+            }
+        }
+    }
+
+    @media screen and (min-width: 3000px) {
+        .carousel-container {
+
+            .slider-container {
+                width: calc(10.52vw - 18px);
+                border: 1px solid red;
+                margin: auto;
+
+                .image-container {
+                    width: calc(10.52vw - 22px);
+                    height: 5.26vw;
+                    display: flex;
+                    align-items: center;
+
+                    img {
+                        width: 100%;
+                    }
                 }
             }
         }
